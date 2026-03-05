c94a039fc13a9c3cda4a7ef5d5131f301c95da1e
김지성 <tak15423@naver.com>
2026-03-05 12:30:46 +0900

    chapter 1

--- src/main/java/me/jskim/Main.java
+++ src/main/java/me/jskim/Main.java
@@ -1,10 +1,10 @@
-package me.jskim.springbootdeveloper;
+package me.jskim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class Main {  // 이름도 SpringBootDeveloperApplication로 바꾸면 더 깔끔
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
