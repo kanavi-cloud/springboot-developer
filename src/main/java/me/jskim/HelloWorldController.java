76f2d2da7bfc19fee25429f3ddc2d44a0f0b6f6e
김지성 <tak15423@naver.com>
2026-03-05 11:47:20 +0900

    chapter 0

---
+++ src/main/java/me/jskim/HelloWorldController.java
@@ -0,0 +1,10 @@
+package me.jskim;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HelloWorldController {
+    return new Student(firstName,lastName);
+}
+
+@GetMapping("/student/{Firstname}/{lastname}")
+public
