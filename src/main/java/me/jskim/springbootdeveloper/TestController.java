875acc29fe3da3388a2adef97afaef6d6a6556bf
김지성 <tak15423@naver.com>
2026-03-17 15:30:59 +0900

    spring JPA , h2 , lombok , data.sql , application.yml , Member , MemberRepository 등

--- src/main/java/me/jskim/springbootdeveloper/TestController.java
+++ src/main/java/me/jskim/springbootdeveloper/TestController.java
@@ -1,7 +1,10 @@
 package me.jskim.springbootdeveloper;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,7 +15,11 @@ public class TestController {
     private TestService testService;
     @GetMapping("/test")
 //    @ResponseBody
-    public List<Member> getAllMembers() {
-        return testService.getAllMembers();
+    public ResponseEntity<List<Member>> getAllMembers() {
+        return ResponseEntity.ok(testService.getAllMembers());
+    }
+    @PostMapping("/test")
+    public ResponseEntity<Member> createMember(@RequestBody Member member) {
+       return ResponseEntity.ok(testService.saveMember(member));
     }
 }
--- src/main/java/me/jskim/springbootdeveloper/TestService.java
+++ src/main/java/me/jskim/springbootdeveloper/TestService.java
@@ -13,4 +13,8 @@ public class TestService {
     public List<Member> getAllMembers() {
         return memberRepository.findAll();
     }
+
+    public Member saveMember(Member member) {
+       return memberRepository.save(member);
+    }
 }
