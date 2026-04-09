7e0c5e8199209e3880e4f1b2ccdc1efc39aaa17a
김지성 <tak15423@naver.com>
2026-04-09 11:47:30 +0900

    refactor: optimize business logic and package structure

--- src/main/java/me/scpark/springdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/scpark/springdeveloper/SpringBootDeveloperApplication.java
@@ -1,10 +1,10 @@
-package me.jskim.springbootdeveloper;
+package me.scpark.springdeveloper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+            SpringApplication.run(SpringBootDeveloperApplication.class, args);
     }
 }
